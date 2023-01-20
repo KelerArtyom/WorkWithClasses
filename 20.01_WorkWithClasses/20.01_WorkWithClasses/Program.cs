fa49a56906fab64e3e51fec00b84493fa562b8de
KelerArtyom <113889007+KelerArtyom@users.noreply.github.com>
2023-01-20 09:46:28 +0300

    Add files via upload

---
+++ 20.01_WorkWithClasses/20.01_WorkWithClasses/Program.cs
@@ -0,0 +1,11 @@
+﻿using _20._01_WorkWithClasses;
+
+Cars[] speed = new Cars[5];
+Random rand = new Random();
+
+for (int i = 0; i < speed.Length; i++)
+{
+    speed[i] = new Cars(rand.Next(200), rand.Next(200), rand.Next(200));
+    Console.WriteLine(speed[i]);
+}
+
